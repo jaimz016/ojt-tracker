bf76c6910644f7f9502e848abe42001a412971bd
jaimz016 <jamesfrong16@gmail.com>
2026-03-31 09:58:48 +0800

    Add files via upload

---
+++ ojt-sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ojt-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './ojt-manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
